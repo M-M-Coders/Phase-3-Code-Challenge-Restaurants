06bf56923f02aa0b38462d5211da695a18260e4b
ecee <you@example.com>
2023-12-17 11:44:06 +0300

    Done

--- seed.py
+++ seed.py
@@ -2,7 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from lib.models import Base, Restaurant, Customer, Review  
 
-def seed_data(session):
+# Create an SQLite in-memory database for testing
+engine = create_engine('sqlite:///migrations_test.db')
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Seed data
+def seed_data():
     # Create some restaurants
     restaurant1 = Restaurant(name="Kempinski", price=4000)
     restaurant2 = Restaurant(name="Food court", price=2000)
@@ -21,7 +30,11 @@ def seed_data(session):
     # Commit the changes to the database
     session.commit()
 
-def query_data(session):
+# Call the seed_data function to populate the database
+seed_data()
+
+# Example usage
+def query_data():
     # Query all restaurants
     all_restaurants = session.query(Restaurant).all()
     print("All Restaurants:")
@@ -40,18 +53,8 @@ def query_data(session):
     for review in all_reviews:
         print(review.full_review())
 
-if __name__ == "__main__":
-    # Create an SQLite in-memory database for testing
-    engine = create_engine('sqlite:///migrations_test.db')
-    Base.metadata.create_all(engine)
+# Call the query_data function to retrieve and print data
+query_data()
 
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=engine)
-
-    # Use a context manager for the session
-    with Session() as session:
-        # Call the seed_data function to populate the database
-        seed_data(session)
-
-        # Call the query_data function to retrieve and print data
-        query_data(session)
+# Close the session
+session.close()
